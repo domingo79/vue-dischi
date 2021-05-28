cd43f98cb4617af980b505526538f90ed8edb799
domingo79 <andria140779@yahoo.it>
2021-05-28 21:01:36 +0200

    bonus-1

---
+++ bonus/asset/js/main.js
@@ -0,0 +1,34 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        url: 'https://flynn.boolean.careers/exercises/api/array/music',
+        response: '', //include tutto l'oggetto
+        generi: [],
+        genereSelezionato: '',
+        // genereAll: 'all'
+    },
+    methods: {
+        getGenere() {
+            let ele = []
+            for (let index = 0; index < this.response.length; index++) {
+                ele.push(this.response[index].genre)
+                //console.log(ele);
+            }
+            this.generi = ele.filter((el, index) => {
+                return ele.indexOf(el) === index;
+            })
+        }
+
+    },
+    mounted() {
+        axios
+            .get(this.url)
+            .then(resp => {
+                this.response = resp.data.response;
+                //console.log('stampa risposta', resp);
+            })
+            .catch(e => {
+                console.error(e);
+            })
+    }
+});
---
+++ bonus/istruzioni.md
@@ -0,0 +1,4 @@
+# BONUS: 
+
+- Creare una select con tutti i generi dei dischi. 
+- In base a cosa scegliamo nella select, vedremo i corrispondenti cd.
