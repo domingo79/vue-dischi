64f1614174b2a4360ced615536473eca6c4cec70
domingo79 <andria140779@yahoo.it>
2021-05-28 17:59:11 +0200

    card-album

---
+++ Vue-Dischi/asset/js/main.js
@@ -0,0 +1,21 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        url: 'https://flynn.boolean.careers/exercises/api/array/music',
+        response: '', //include tutto l'oggetto
+
+    },
+    methods: {},
+    mounted() {
+        axios
+            .get(this.url)
+            .then(resp => {
+                this.response = resp.data.response
+                console.log('stampa risposta', resp);
+            })
+            .catch(e => {
+                console.error(e);
+            })
+
+    }
+});
